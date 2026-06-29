f74a53eb2e7c3e4dd5caae395d36039dd07ed723
kv <kavyajain292005@gmail.com>
2026-06-29 07:58:05 +0000

    basic complete

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,33 @@
+// dates
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2026, 0, 15)
+console.log(myCreateDate.toDateString());
+
+let myDatee = new Date(2023,0,23,5,3)
+console.log(myDatee.toDateString());
+
+let myCDate = new Date("2023-01-14")
+console.log(myCDate.toDateString());
+
+let myCDates = new Date("01-14-2026")
+console.log(myCDates.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCDates.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
